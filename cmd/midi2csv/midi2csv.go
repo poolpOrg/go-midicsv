67e139a93c01a89e69ad37fab26f5db057ac32cf
Gilles Chehade <gilles@poolp.org>
2023-03-30 23:47:31 +0200

    work in progress, converts mid to csv and the other way around

--- cmd/midi2csv/midi2csv.go
+++ cmd/midi2csv/midi2csv.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
+	"strconv"
+	"strings"
 
 	"gitlab.com/gomidi/midi/v2/smf"
 )
@@ -12,27 +15,52 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	defer fp.Close()
 
 	x := smf.ReadTracksFrom(fp)
-	fmt.Printf("0,0,Header,%d,%d,%d\n", x.SMF().Format(), len(x.SMF().Tracks), 0)
-	//0, 0, Header, format, nTracks, division
+	nTracks := len(x.SMF().Tracks)
+
+	events := make(map[int][]string)
+	c := make(chan string)
+	go func() {
+		for msg := range c {
+			fields := strings.Split(msg, ",")
+			trackNo, err := strconv.Atoi(fields[0])
+			if err != nil {
+				continue
+			}
+			events[trackNo] = append(events[trackNo], msg)
+		}
+	}()
 
 	x.Do(
 		func(te smf.TrackEvent) {
 			switch te.Message.Type().String() {
+
+			// File Meta-Events
 			case "MetaTrackName":
 				var trackName string
 				te.Message.GetMetaTrackName(&trackName)
-				fmt.Printf("%d,%d,Title_t,%s\n", te.TrackNo, te.AbsTicks, trackName)
+				c <- fmt.Sprintf("%d,%d,Title_t,\"%s\"", te.TrackNo, te.AbsTicks, trackName)
+				// copyright
+				// instrument
+				// marker
+				// cue
 
-			case "MetaSMPTEOffset":
-				var hour uint8
-				var minute uint8
-				var second uint8
-				var frame uint8
-				var fractframe uint8
-				te.Message.GetMetaSMPTEOffsetMsg(&hour, &minute, &second, &frame, &fractframe)
-				fmt.Printf("%d,0,SMPTE_offset,%d,%d,%d,%d,%d\n", te.TrackNo, hour, minute, second, frame, fractframe)
+			case "MetaLyric":
+				var text string
+
+				te.Message.GetMetaLyric(&text)
+				c <- fmt.Sprintf("%d,%d,Lyric_t,%s", te.TrackNo, te.AbsTicks, text)
+				// text
+				//sequencenumber
+
+			case "MetaPort":
+				var port uint8
+
+				te.Message.GetMetaPort(&port)
+				c <- fmt.Sprintf("%d,%d,MIDI_port,%d", te.TrackNo, te.AbsTicks, port)
+				// channel prefix
 
 			case "MetaTimeSig":
 				var numerator uint8
@@ -41,7 +69,7 @@ func main() {
 				var demiSemiQuaverPerQuarter uint8
 
 				te.Message.GetMetaTimeSig(&numerator, &denominator, &clocksPerClick, &demiSemiQuaverPerQuarter)
-				fmt.Printf("%d,%d,Time_signature,%d,%d,%d,%d\n", te.TrackNo, te.AbsTicks, numerator, denominator, clocksPerClick, demiSemiQuaverPerQuarter)
+				c <- fmt.Sprintf("%d,%d,Time_signature,%d,%d,%d,%d", te.TrackNo, te.AbsTicks, numerator, denominator, clocksPerClick, demiSemiQuaverPerQuarter)
 
 			case "MetaKeySig":
 				var key uint8
@@ -56,52 +84,39 @@ func main() {
 				} else {
 					mode = "minor"
 				}
-				fmt.Printf("%d,%d,Key_signature,%d,\"%s\"\n", te.TrackNo, te.AbsTicks, key, mode)
+				c <- fmt.Sprintf("%d,%d,Key_signature,%d,\"%s\"", te.TrackNo, te.AbsTicks, key, mode)
 
 			case "MetaTempo":
 				var bpm float64
 
 				te.Message.GetMetaTempo(&bpm)
 				tempo := float64(60_000_000) / bpm
-				fmt.Printf("%d,%d,Tempo,%d\n", te.TrackNo, te.AbsTicks, int(tempo))
+				c <- fmt.Sprintf("%d,%d,Tempo,%d", te.TrackNo, te.AbsTicks, int(tempo))
 
-			case "MetaPort":
-				var port uint8
+			case "MetaSMPTEOffset":
+				var hour uint8
+				var minute uint8
+				var second uint8
+				var frame uint8
+				var fractframe uint8
 
-				te.Message.GetMetaPort(&port)
-				fmt.Printf("%d,%d,MIDI_port,%d\n", te.TrackNo, te.AbsTicks, port)
+				te.Message.GetMetaSMPTEOffsetMsg(&hour, &minute, &second, &frame, &fractframe)
+				c <- fmt.Sprintf("%d,0,SMPTE_offset,%d,%d,%d,%d,%d", te.TrackNo, hour, minute, second, frame, fractframe)
 
-			case "MetaLyric":
-				var text string
-
-				te.Message.GetMetaLyric(&text)
-				fmt.Printf("%d,%d,Lyric_t,%s\n", te.TrackNo, te.AbsTicks, text)
+				// sequencer specific
+				// unknown meta event
 
 			case "MetaEndOfTrack":
-				fmt.Printf("%d,%d,End_track\n", te.TrackNo, te.AbsTicks)
+				c <- fmt.Sprintf("%d,%d,End_track", te.TrackNo, te.AbsTicks)
 
-			case "ProgramChange":
-				var channel uint8
-				var program uint8
-
-				te.Message.GetProgramChange(&channel, &program)
-				fmt.Printf("%d,%d,Program_c,%d,%d\n", te.TrackNo, te.AbsTicks, channel, program)
-
-			case "ControlChange":
-				var channel uint8
-				var controller uint8
-				var value uint8
-
-				te.Message.GetControlChange(&channel, &controller, &value)
-				fmt.Printf("%d,%d,Control_c,%d,%d,%d\n", te.TrackNo, te.AbsTicks, channel, controller, value)
-
+			// Channel Events
 			case "NoteOn":
 				var channel uint8
 				var key uint8
 				var velocity uint8
 
 				te.Message.GetNoteOn(&channel, &key, &velocity)
-				fmt.Printf("%d,%d,Note_on_c,%d,%d,%d\n", te.TrackNo, te.AbsTicks, channel, key, velocity)
+				c <- fmt.Sprintf("%d,%d,Note_on_c,%d,%d,%d", te.TrackNo, te.AbsTicks, channel, key, velocity)
 
 			case "NoteOff":
 				var channel uint8
@@ -109,13 +124,56 @@ func main() {
 				var velocity uint8
 
 				te.Message.GetNoteOff(&channel, &key, &velocity)
-				fmt.Printf("%d,%d,Note_off_c,%d,%d,%d\n", te.TrackNo, te.AbsTicks, channel, key, velocity)
+				c <- fmt.Sprintf("%d,%d,Note_off_c,%d,%d,%d", te.TrackNo, te.AbsTicks, channel, key, velocity)
+
+			case "PitchBend":
+				var channel uint8
+				var relative int16
+				var absolute uint16
+
+				te.Message.GetPitchBend(&channel, &relative, &absolute)
+				c <- fmt.Sprintf("%d,%d,Control_c,%d,%d", te.TrackNo, te.AbsTicks, channel, relative)
+
+			case "ControlChange":
+				var channel uint8
+				var controller uint8
+				var value uint8
+
+				te.Message.GetControlChange(&channel, &controller, &value)
+				c <- fmt.Sprintf("%d,%d,Control_c,%d,%d,%d", te.TrackNo, te.AbsTicks, channel, controller, value)
+
+			case "ProgramChange":
+				var channel uint8
+				var program uint8
+
+				te.Message.GetProgramChange(&channel, &program)
+				c <- fmt.Sprintf("%d,%d,Program_c,%d,%d", te.TrackNo, te.AbsTicks, channel, program)
+			//case "ChannelAftertouch"
+			//case "PolyAftertouch"
+
+			// System Exclusive Events
+			// System_exclusive (length, data, ...)
+			// System_exclusive_packet (length, data, ...)
 
 			default:
-				fmt.Printf("[%v] @%vms %s\n", te.TrackNo, te.AbsMicroSeconds/1000, te.Message.String())
+				fmt.Printf("[%v] @%vms %s", te.TrackNo, te.AbsMicroSeconds/1000, te.Message.String())
 				panic("unknown type: " + te.Message.Type().String())
 			}
 		},
 	)
+
+	trackOrder := make([]int, 0)
+	for trackNo := range events {
+		trackOrder = append(trackOrder, trackNo)
+	}
+	sort.Slice(trackOrder, func(i, j int) bool { return trackOrder[i] < trackOrder[j] })
+
+	fmt.Printf("0,0,Header,%d,%d,%d\n", x.SMF().Format(), nTracks, x.SMF().TimeFormat)
+	for _, trackNo := range trackOrder {
+		fmt.Printf("%d,0,Start_track\n", trackNo)
+		for _, event := range events[trackNo] {
+			fmt.Println(event)
+		}
+	}
 	fmt.Printf("0,0,End_of_file\n")
 }
