c54406ba1e4385ea694f8222a2e69fa7217af2c9
Gilles Chehade <gilles@poolp.org>
2023-04-01 11:48:07 +0200

    add opt args

--- cmd/midi2csv/midi2csv.go
+++ cmd/midi2csv/midi2csv.go
@@ -1,23 +1,47 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
+	"log"
 	"os"
 
 	"github.com/poolpOrg/go-midicsv/encoding"
 )
 
 func main() {
-	fp, err := os.Open(os.Args[1])
-	if err != nil {
-		panic(err)
+	var opt_file string
+
+	flag.StringVar(&opt_file, "out", "", "output file (.csv)")
+	flag.Parse()
+
+	if flag.NArg() == 0 {
+		log.Fatal("need a source file to process")
 	}
 
-	records, err := encoding.NewEncoder(fp).Encode()
+	ifp, err := os.Open(flag.Arg(0))
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+	}
+	defer ifp.Close()
+
+	records, err := encoding.NewEncoder(ifp).Encode()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var ofp io.Writer
+	if opt_file == "" {
+		ofp = os.Stdout
+	} else {
+		ofp, err = os.Create(opt_file)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 	for _, record := range records {
-		fmt.Println(record)
+		fmt.Fprintf(ofp, "%s\n", record)
 	}
+	os.Exit(0)
 }
